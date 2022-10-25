14b2596f49e14ae7109114f5362b45fca7410f2f
Thomas Json <joogletom8@gmail.com>
2022-10-25 16:24:26 +0200

    Changes

--- app/Table/Article.php
+++ app/Table/Article.php
@@ -16,12 +16,12 @@ class Article
     }
 
     public function getUrl(){      
-        return 'index.php?=p=article&id=' . $this->id;
+        return 'index.php?p=article&id=' . $this->id;
     }
 
     public function getExtrait(){
         $html = '<p>' . substr($this->contenu, 0, 100) . ' ...</p>';
-        // $html .= '<p><a href="' . $this->getURL() . '">Voir la suite</a></p>';
+        $html .= '<p><a href="' . $this->getUrl() . '">Voir la suite</a></p>';
         return $html;
     }
 }
